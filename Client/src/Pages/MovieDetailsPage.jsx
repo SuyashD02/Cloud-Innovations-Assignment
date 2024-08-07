4e951d1982f13652bb14efad8e016b59bfa375ba
Suyash Dattawade <suyashdattawade007@gmail.com>
2024-08-07 18:05:10 +0530

    Assignment

---
+++ Client/src/Pages/MovieDetailsPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const MovieDetailsPage = () => {
+  const { id } = useParams();
+  const movies = useSelector((state) => state.movies.movies);
+  const movie = movies.find((movie) => movie.id === parseInt(id));
+  console.log('Movies state:', movies);
+  console.log('Selected movie:', movie);
+
+  if (!movie) return <p>Movie not found</p>;
+
+  return (
+    <div>
+      <h3 className="movie-id">{movie.id}</h3>
+      <img src={movie.Poster} alt={movie.Title} className="movie-image" />
+      <h2 className="movie-name">{movie.Title}</h2>
+      <p className="movie-year">{movie.Year}</p>
+      <p className="movie-type">{movie.Type}</p>
+      <p className="movie-rating">{movie.rating}</p>
+    </div>
+  );
+};
+export default MovieDetailsPage;
